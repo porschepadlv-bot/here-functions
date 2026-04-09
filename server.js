23e2f4efdd8544815ebe5c1a00b1d0e97fadd3ef
Kenny Austin <porschepadlv@gmail.com>
2026-04-09 09:46:11 -0700

    force overwrite server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-cat > server.js
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/reply", (req, res) => {
+return res.json({
+replies: [
+"REAL SERVER FILE",
+"SERVER.JS IS ACTIVE",
+"THIS IS THE RIGHT BACKEND"
+]
+});
+});
+
+app.get("/", (req, res) => {
+res.send("Server running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+console.log("Server running on port " + (process.env.PORT || 3000));
+});
